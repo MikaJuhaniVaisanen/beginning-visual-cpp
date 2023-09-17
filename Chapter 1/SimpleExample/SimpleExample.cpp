bd33e86d060b76d6944b08210f69048d8f5450e7
Mika <vaisasenmika89@gmail.com>
2023-09-17 17:38:02 +0300

    A simple console program

---
+++ Chapter 1/SimpleExample/SimpleExample.cpp
@@ -0,0 +1,13 @@
+// SimpleExample.cpp -- A simple console program
+#include <iostream>			// Basic input and output library
+
+int main()
+{
+	std::cout << "This is a simple program that outputs some text." << std::endl;
+	std::cout << "You can output more lines of text" << std::endl;
+	std::cout << "just by repeating the output statement like this." << std::endl;
+
+	system("pause");
+
+	return 0;				// Return to the operating system
+}
